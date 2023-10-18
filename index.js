cbbe584d99b815d16a9a23792e6b4f728304c259
Rodrigo Lopes <60493039+RodrigoLopes76@users.noreply.github.com>
2023-10-18 09:43:53 -0300

    Update index.js
    tentando concatenar

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 
 //var XP;
 //XP = prompt("Digite quanto XP o seu Herói possui: ")
-
+let nickname = "Pikatchu"
 let xp = 1500
 
 switch (xp){
     case xp < 1000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Ferro")
+        console.log(`O Herói de nome ${nickname.concat( )} está no nível de Ferro`)
+        
     case 1001 <= xp < 2000:
         console.log("O Herói de nome ") +nickname ("está no nível de Bronze")
     case 2001 <= xp < 5000:
@@ -25,9 +26,9 @@ switch (xp){
         console.log("O Herói de nome ") +nickname ("está no nível de Radiante")
 
 }
+console.log(`O Herói de nome ${nickname} tem XP de Ouro`);
 
-
-
+let nickname = 
 let xp = 2002
 
 if (xp < 1000){
