5f98fcd2c06907f4285a353d8f5c565f99fe2339
RodrigoLopes76 <rodrigo.coelho.lopes@gmail.com>
2023-10-17 22:23:28 -0300

    teste

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("testando")
+//var nickname;
+//nickname = prompt("Digite o nome do Herói: ")
+
+//var XP;
+//XP = prompt("Digite quanto XP o seu Herói possui: ")
+
+let xp = 1500
+
+switch (xp){
+    case xp < 1000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Ferro")
+    case 1001 <= xp < 2000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Bronze")
+    case 2001 <= xp < 5000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Prata")
+    case 6001 <= xp < 7000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Ouro")
+    case 7001 <= xp < 8000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Platina")
+    case 8001 <= xp < 9000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Ascendente")
+    case 9001 <= xp < 10000:
+        console.log("O Herói de nome ") +nickname ("está no nível de Imortal")
+    case xp > 10001:
+        console.log("O Herói de nome ") +nickname ("está no nível de Radiante")
+
+}
+
+
+
+let xp = 2002
+
+if (xp < 1000){
+        console.log("O Herói de nome Ouro ") ;
+    }else if (1001 <= xp < 2000){
+        console.log("O Herói de nome Prata ") ;
+    }else if (2001 <= xp < 5000){
+        console.log("O Herói de nome Bronze") ;
+    }
