d1652b27dcb6c74f771b98b16a3811325bfac1fa
RodrigoLopes76 <rodrigo.coelho.lopes@gmail.com>
2023-10-18 21:50:10 -0300

    Agora deu certo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-<<<<<<< HEAD
 //Definindo as constantes
+
 const nickname = ("Pikaxu")
 
 const xp = 7150
 
 //Definindo o nível
+
 let nivel = ""
 
 if (xp < 1000) {
@@ -39,48 +39,8 @@ if (xp < 1000) {
 } else {
 
   nivel = "Radiante"
-=======
-//var XP;
-//XP = prompt("Digite quanto XP o seu Herói possui: ")
-let nickname = "Pikatchu"
-let xp = 1500
-
-switch (xp){
-    case xp < 1000:
-        console.log(`O Herói de nome ${nickname.concat( )} está no nível de Ferro`)
-        
-    case 1001 <= xp < 2000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Bronze")
-    case 2001 <= xp < 5000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Prata")
-    case 6001 <= xp < 7000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Ouro")
-    case 7001 <= xp < 8000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Platina")
-    case 8001 <= xp < 9000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Ascendente")
-    case 9001 <= xp < 10000:
-        console.log("O Herói de nome ") +nickname ("está no nível de Imortal")
-    case xp > 10001:
-        console.log("O Herói de nome ") +nickname ("está no nível de Radiante")
->>>>>>> cbbe584d99b815d16a9a23792e6b4f728304c259
 
 }
-console.log(`O Herói de nome ${nickname} tem XP de Ouro`);
-
-<<<<<<< HEAD
 //Mensagem de saída
 
 console.log(`O Herói de nome ${nickname} está no nível de ${nivel}`)
-=======
-let nickname = 
-let xp = 2002
-
-if (xp < 1000){
-        console.log("O Herói de nome Ouro ") ;
-    }else if (1001 <= xp < 2000){
-        console.log("O Herói de nome Prata ") ;
-    }else if (2001 <= xp < 5000){
-        console.log("O Herói de nome Bronze") ;
-    }
->>>>>>> cbbe584d99b815d16a9a23792e6b4f728304c259
