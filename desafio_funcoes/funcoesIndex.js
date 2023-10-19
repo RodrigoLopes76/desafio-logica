3f2c7b9b7a2139e37a7a9005734aad99d9a65188
RodrigoLopes76 <rodrigo.coelho.lopes@gmail.com>
2023-10-18 22:46:56 -0300

    erro de function

---
+++ desafio_funcoes/funcoesIndex.js
@@ -0,0 +1,23 @@
+
+
+function rankear(vitorias, derrotas){
+    return vitorias - derrotas
+
+let ranking = (20, 10)
+}
+if (ranking < 10) {
+    rankeadas = "Ferro"
+} else if (ranking <= 20){
+    rankeadas = "Bronze"
+} else if (ranking <= 50){
+    rankeadas = "Prata"
+}else if (ranking <= 80){
+    rankeadas = "Ouro"
+}else if (ranking <= 90){
+    rankeadas = "Diamante"
+}else if (ranking <= 100){
+    rankeadas = "Lendário"
+}else {
+    rankeadas = "Imortal"
+}
+console.log(`O Herói tem saldo de vitórias de ${ranking} está no nível ${rankeadas}`)
