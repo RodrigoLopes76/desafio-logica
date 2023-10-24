a95a0904f9fdf8cd6410854eed331ebb9bb410ff
RodrigoLopes76 <rodrigo.coelho.lopes@gmail.com>
2023-10-23 22:25:29 -0300

    deu certo!!

--- desafio_heroi/heroiIndex.js
+++ desafio_heroi/heroiIndex.js
@@ -3,36 +3,22 @@ class heroi{
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
-    }
-
-    if (tipo = mago){
-        atack = "magia"
-    }else (tipo = guerreiro){
-        atack = "espada"
-    }else (tipo = monge){
-        atack = "artes marciais"
-    }else (tipo = ninja){
-        atack = "shuriken"
-    }
+        this.atack= {
+            mago:"magia",
+            guerreiro: "espada",
+            monge: "artes marciais",
+            ninja: "shuriken",
+        }
+        }
     atackMode(){
-        console.log(`o ${tipo} atacou usando ${atack}`)
+        console.log(`O ${this.tipo} atacou usando ${this.atack[this.tipo]}`)
     }
 }
-let heroiMago = new heroi(Jão, 23, mago)
-let heroiGuerreiro = new heroi(Zé, 50, guerreiro)
-let heroiMonge = new heroi(Yoda, 1200, monge)
-let heroiNinja = new heroi(ChunLi, 12, ninja)
-
-
-
-/*lém disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
-
-- exibir a mensagem: "o {tipo} atacou usando {ataque}")
-- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
-- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
-
-se mago -> no ataque exibir (usou magia)
-se guerreiro -> no ataque exibir (usou espada)
-se monge -> no ataque exibir (usou artes marciais)
-se ninja -> no ataque exibir (usou shuriken)
-"o {tipo} atacou usando {ataque}"
+let heroiMago = new heroi("Jão", 23, "mago")
+let heroiGuerreiro = new heroi("Zé", 50, "guerreiro")
+let heroiMonge = new heroi("Yoda", 1200, "monge")
+let heroiNinja = new heroi("ChunLi", 12, "ninja")
+heroiMago.atackMode()
+heroiGuerreiro.atackMode()
+heroiMonge.atackMode()
+heroiNinja.atackMode()
